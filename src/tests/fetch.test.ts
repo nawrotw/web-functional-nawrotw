ec287c30a464345e2a17d53a990854bc41dc9c64
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-05-08 06:59:07 +0200

    fetch.test.ts fix

--- src/tests/fetch.test.ts
+++ src/tests/fetch.test.ts
@@ -1,33 +1,32 @@
-import { setupWorker } from 'msw/browser';
-import {
-  beforeAll,
-  describe,
-  expect,
-  it,
-} from 'vitest';
+import { setupServer } from 'msw/node'
+import { beforeAll, describe, expect, it, afterEach, } from 'vitest';
 
 import { fetchContent } from '../lib/content';
 import { handlers } from './mocks/handlers';
 
-const server = setupWorker();
+const server = setupServer();
 
 describe("fetchContent Test Suite", () => {
   beforeAll(() => {
-    server.start({ onUnhandledRequest: "error" });
-    return () => server.stop();
+    server.listen({ onUnhandledRequest: "error" });
+    return () => server.close();
+  });
+  afterEach(() => {
+    server.resetHandlers();
   });
 
   it("fetches content from url and returns the content as string", async () => {
     server.use(...handlers.success);
     const content = await fetchContent();
     expect(content).toBeTypeOf("string");
-    server.resetHandlers();
+    expect(content).toEqual(
+      "<speak><s>This is a sentence.</s><s>This is another sentence</s>Some more text<s>This is a longer piece of content</s></speak>"
+    );
   });
 
   it('returns a fixed error string on failure "<speak><s>There was an error</s></speak>" ', async () => {
     server.use(...handlers.error);
     const content = await fetchContent();
     expect(content).equals("<speak><s>There was an error</s></speak>");
-    server.resetHandlers();
   });
 });
