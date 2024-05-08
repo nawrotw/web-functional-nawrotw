7b98958b289c1201e8890803b21176b31994c7e1
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-05-08 07:31:59 +0200

    cleanings

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -18,32 +18,33 @@ const fetchContent = async (url = API_URL): Promise<string> => {
   return Promise.resolve("<speak><s>There was an error</s></speak>");
 };
 
+// Creating RegExp is an expensive task, so it is better to created them once and reuse.
+const sentenceSplitRegExp = new RegExp('<s>(.+?)<\\/s>', 'g');
+const searchRegExp = /<\/?s>/g;
 /**
  * Parse the content into sentences, and return an array of sentences.
  *
  * input: "<speak><s>This is a sentence.</s><s>This is another sentence</s></speak>",
  * sentences: ['This is a sentence.', 'This is another sentence']
  */
-// Event if this code work, regexp can be optimized, but with more research. And right not I dont have a time,
-// I will back to it at the end if possible
-const sentenceSplitRegExp = new RegExp('<s>(.+?)<\\/s>', 'g')
 const parseContentIntoSentences = (content: string): string[] => {
   if (!content) {
     throw new Error("This is not valid ssml");
   }
 
-  const sentencesMatch = content.match(/<speak>(.*)<\/speak>/);
-  if (!sentencesMatch || sentencesMatch?.length === 0) {
+  const speakContentMatch = content.match(/<speak>(.*)<\/speak>/);
+  if (!speakContentMatch || speakContentMatch?.length === 0) {
     throw new Error("This is not valid ssml");
-    // return ['There was an error'];
   }
-  // now split sentences
-  return sentencesMatch.slice(1).flatMap(sentences => {
-    const match = sentences.match(sentenceSplitRegExp);
-    return match?.map(sentence => sentence
-      .replace(/<\/?s>/g, '')
-    )
-  })
+  // now parse sentences from speak element/s
+  return speakContentMatch.slice(1).flatMap(speakContent => {
+    return speakContent
+        .match(sentenceSplitRegExp)
+        ?.map(sentence =>
+          sentence.replace(searchRegExp, '')
+        );
+    }
+  )
     .filter(Boolean) as string[];
 };
 
